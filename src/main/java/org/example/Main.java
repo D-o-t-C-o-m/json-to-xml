01f49d63fb48653c08b20155aa323dcad8fc2964
valkuros@gmail.com <valkuros@gmail.com>
2025-01-10 21:37:47 -0500

    Abstracted read file into a separate method.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,34 +6,19 @@ import java.util.Scanner;
 
 
 public class Main {
-public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-	Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 
-	System.out.println("JSON TO XML CONVERSION\n");
+		System.out.println("JSON TO XML CONVERSION\n");
 
-	System.out.print("Enter Filepath of the item to convert: ");
-	String filePath = scanner.nextLine();
-	scanner.close();
+		System.out.print("Enter Filepath of the item to convert: ");
+		String filePath = scanner.nextLine();
+		scanner.close();
 
-	String newFilePath = filePath.replace(".json", ".xml");
+		String newFilePath = filePath.replace(".json", ".xml");
 
-	try {
-		File inputFile = new File(filePath);
-
-		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile), StandardCharsets.UTF_8));
-		StringBuilder builder = new StringBuilder();
-
-		String line;
-
-		while ((line = reader.readLine()) != null) {
-			builder.append(line);
-		}
-		String inputJson = builder.toString().trim();
-
-		reader.close();
-
-		inputJson = inputJson.replace(" ", "");
+		String inputJson = readFile(filePath);
 		String outputXml = U.jsonToXml(inputJson);
 
 		System.out.println(outputXml);
@@ -42,11 +27,28 @@ public static void main(String[] args) throws IOException {
 		outStream.write(outputXml.getBytes());
 		outStream.close();
 
-	} catch (FileNotFoundException e) {
-		System.out.println("File Not Found");
-		e.printStackTrace();
-	}
+	System.out.println("Processing complete. File saved to "+newFilePath);
+}
 
-	System.out.println("Processing complete. File saved to " + newFilePath);
+public static String readFile(String filePath) throws IOException {
+	File inputFile = new File(filePath);
+
+	BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile), StandardCharsets.UTF_8));
+	StringBuilder builder = new StringBuilder();
+
+	String line;
+
+	while ((line = reader.readLine()) != null) {
+		builder.append(line);
 	}
+	String inputJson = builder.toString().trim();
+
+	reader.close();
+
+	inputJson = inputJson.replace(" ", "");
+	return inputJson;
+
+}
+
+
 }
